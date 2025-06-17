e977232e118fcf8100c2edb81a65d986d1bc18ba
chathurasandaruwan <chathurasandaruwan122@gamail.com>
2025-06-17 14:13:05 +0530

    create add task page temp

---
+++ src/app/create/page.tsx
@@ -0,0 +1,9 @@
+export default function CreateTask() {
+  return (
+    <main className="container mx-auto p-4 max-w-4xl">
+      <div className="flex items-center justify-between mb-8">
+        <h1 className="text-2xl font-bold mb-6 text-gray-900">Create New Task</h1> 
+      </div>
+    </main>
+  );
+}
